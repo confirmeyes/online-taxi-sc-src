bfc9228e4d049e2da823072ad95213bc2d5bff49
confirmeyes <confirmeyes@163.com>
2020-07-08 18:59:19 +0800

    .

---
+++ cloud-zuul/src/main/java/filter/GrayFilter.java
@@ -0,0 +1,32 @@
+package filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.exception.ZuulException;
+
+/**
+ * @author WIN10 .
+ * @create 2020-07-08-16:40 .
+ * @description .
+ */
+
+public class GrayFilter extends ZuulFilter {
+    @Override
+    public String filterType() {
+        return null;
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return false;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        return null;
+    }
+}
